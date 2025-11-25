9208dfbe0c3fcb97b973c7e4d5dff50cb0d230a0
bi3mer <colan@comcast.net>
2025-11-25 09:56:25 -0500

    basic set up for downloading github releases

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+func github_release_url(repo, tag, filename string) string {
+
+	switch runtime.GOOS {
+	case "darwin":
+		return fmt.Sprintf("https://github.com/bi3mer/%s/releases/download/%s/mac-%s",
+			repo, tag, filename)
+	case "windows":
+		return fmt.Sprintf("https://github.com/bi3mer/%s/releases/download/%s/win-%s.exe",
+			repo, tag, filename)
+	}
+
+	fmt.Printf("Unsupported OS type: %s\n", runtime.GOOS)
+	os.Exit(1)
+	return "FAIL"
+}
+
+type Game struct {
+	Name     string
+	URL      string
+	FileName string
+}
+
+func fetch_game(g Game) bool {
+	resp, err := http.Get(g.URL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fetch error fetching %s: %v\n", g.URL, err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "Bad status for %s: %d\n", g.URL, resp.StatusCode)
+		return false
+	}
+
+	out, err := os.Create(g.FileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create file %s: %v\n", g.FileName, err)
+		return false
+	}
+	defer out.Close()
+
+	if _, err := io.Copy(out, resp.Body); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed writing %s: %v\n", g.FileName, err)
+		return false
+	}
+
+	if runtime.GOOS == "darwin" {
+		if err := os.Chmod(g.FileName, 0755); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to chmod %s: %v\n", g.FileName, err)
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Printf("Colan's Game Library\n")
+
+	games := []Game{
+		{
+			Name:     "Snake",
+			URL:      github_release_url("c-snake", "v0.0.0", "snake"),
+			FileName: "snake",
+		},
+		{
+			Name:     "Pong",
+			URL:      github_release_url("raylib-pong", "v0.0.0", "pong"),
+			FileName: "pong",
+		},
+	}
+
+	// https://github.com/bi3mer/raylib-pong/releases/download/v0.0.0/mac-pong
+
+	fetch_game(games[0])
+
+}
